6634be491c208355df48788a47472863ed43d241
Pasindu De Silva <ppasindud@gmail.com>
2017-02-07 04:46:20 +0800

    make accuracy -1 when there is a error running the algo, run different methods and lamdas

--- assignment_3.py
+++ assignment_3.py
@@ -14,83 +14,84 @@ export OCTAVE_EXECUTABLE=/usr/local/octave/3.8.0/bin/octave-3.8.0
 
 lambdanum = 1.0
 features = [1, 2, 3,4,5,6,7,8,9,10,
-			11,12,13,14,15,16,17,18,19,20,
-			21,22,23,24,25,26,27,28,29,30,
-			31,32,33,34,35,36,37,38,39,40,
-			41,42,43,44,45,46,47,48,49,50,
-			51,52,53,54,55,56,57];
-MLPATH="/Users/Pasindu/Downloads/ML_Assignment/mltool-master/classifiers"
+                        11,12,13,14,15,16,17,18,19,20,
+                        21,22,23,24,25,26,27,28,29,30,
+                        31,32,33,34,35,36,37,38,39,40,
+                        41,42,43,44,45,46,47,48,49,50,
+                        51,52,53,54,55,56,57];
+#MLPATH="/Users/Pasindu/Downloads/ML_Assignment/mltool-master/classifiers"
 FEATURE_COMBINATION_FILE = "perms10.txt"
-PATH_TO_RESULTS_FILE = "/Users/Pasindu/Downloads/ML_Assignment/results1.txt"
+#PATH_TO_RESULTS_FILE = "/Users/Pasindu/Downloads/ML_Assignment/results1.txt"
+
+#VM
+MLPATH="/home/ppasindud/MLAssignment3-master/mltool-master/classifiers"
+PATH_TO_RESULTS_FILE = "/home/ppasindud/MLAssignment3-master/results_ubuntu_2.txt"
 
 def multi_parrlle_spam_one_wrapper(args):
    return parrlle_spam_one(*args)
 
 def parrlle_spam_one(feature_set_no, features, method, lamba):
-	oc = oct2py.Oct2Py()
-	oc.addpath(MLPATH)
-	[accuracy, sensitivity] = oc.run_spam_algo(method, lamba, features)
-	write_result(feature_set_no, features, method, lamba, accuracy, sensitivity)
+        try:
+                oc = oct2py.Oct2Py()
+                oc.addpath(MLPATH)
+                [accuracy, sensitivity] = oc.run_spam_algo(method, lamba, features)
+
+        except:
+                accuracy = -1
+                sensitivity = -1
+        write_result(feature_set_no, features, method, lamba, accuracy, sensitivity)
 
 def write_result(feature_set_no, features, method, lamba, accuracy, sensitivity):
-	args = [feature_set_no, features, method, lamba, accuracy, sensitivity]
-	args = [str(e) for e in args]
-	output = "\t".join(args) + "\n"
-	print output
-	f = open(PATH_TO_RESULTS_FILE, 'ab')
-	f.write(output)
-	f.close()
-
+        args = [feature_set_no, features, method, lamba, accuracy, sensitivity]
+        args = [str(e) for e in args]
+        output = "\t".join(args) + "\n"
+        print feature_set_no
+        #print output
+        f = open(PATH_TO_RESULTS_FILE, 'ab')
+        f.write(output)
+        f.close()
+        print feature_set_no
 def parrlle_spam():
-	i = 0
-	jobs = []
-	all_args = []
-	pool_use = True
-
-	for x in open(FEATURE_COMBINATION_FILE):
-		feats = []
-		feats.append([int(e) for e in x.split(",")])
-		method = "logistic"
-		# print feats[0]
-		all_args.append((i, feats[0], method, lambdanum))
-		if not pool_use:
-			p = Process(target=parrlle_spam_one, args=(i, feats[0], method, lambdanum))
-			jobs.append(p)
-			p.start()
-		i +=1
-
-	pool = Pool(2)
-
-	print all_args
-	if pool_use:
-		p = Pool(2)
-		p.map(multi_parrlle_spam_one_wrapper, all_args)
-	
-	# for i in xrange(0, len(all_args)):
-	# 	pool.apply_async(parrlle_spam_one, args=(all_args,))
-	# 	print i
-	# pool.close()
-	# pool.join()
-
-	# Wait for everyone to finish
-	for job in jobs:
-		job.join()
-
+        i = 0
+        jobs = []
+        all_args = []
+        pool_use = True
+        methods = ["svm"]
+        lamdas = [1.0, 0.9]
+        for x in open(FEATURE_COMBINATION_FILE):
+                for method in methods:
+                        for lm in lamdas:
+                                feats = []
+                                feats.append([int(e) for e in x.split(",")])
+                                all_args.append((i, feats[0], method, lambdanum))
+                                if not pool_use:
+                                        p = Process(target=parrlle_spam_one, args=(i, feats[0], method, lambdanum))
+                                        jobs.append(p)
+                                        p.start()
+                                i +=1
+        #print all_args
+        if pool_use:
+                p = Pool(7)
+                p.map(multi_parrlle_spam_one_wrapper, all_args)
+        # for i in xrange(0, len(all_args)):
+        #       pool.apply_async(parrlle_spam_one, args=(all_args,))
+        #       print i
+        # pool.close()
+        # pool.join()
+        # Wait for everyone to finish
+        for job in jobs:
+                job.join()
 def main(argv):
-	parrlle_spam()
-	exit(0)
-
-	# Comment above to run test
-	oc = oct2py.Oct2Py()
-	oc.addpath(MLPATH)
-	method = "logistic"
-	
-	# Test wether coloums or rows
-	[accuracy, sensitivity, feats] = oc.run_spam_algo(method, lambdanum, [1,3])
-	print accuracy,sensitivity, feats
-
-	[accuracy, sensitivity, feats] = oc.run_spam_algo(method, lambdanum, [[1,3]])
-	print accuracy,sensitivity, feats
-
+        parrlle_spam()
+        exit(0)
+        # Comment above to run test
+        oc = oct2py.Oct2Py()
+        oc.addpath(MLPATH)
+        method = "logistic"
+        # Test wether coloums or rows
+        [accuracy, sensitivity, feats] = oc.run_spam_algo(method, lambdanum, [1,3])
+        print accuracy,sensitivity, feats
+        [accuracy, sensitivity, feats] = oc.run_spam_algo(method, lambdanum, [[1,3]])
+        print accuracy,sensitivity, feats
 if __name__ == "__main__":
-	main(sys.argv)
+        main(sys.argv)
