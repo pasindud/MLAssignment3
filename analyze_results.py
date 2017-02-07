a552c8d79a8d6d93b4f5018c0301e6cf68e3fe2f
Pasindu De Silva <pasindu@google.com>
2017-02-08 00:09:56 +0800

    added analuze score logic

--- analyze_results.py
+++ analyze_results.py
@@ -3,7 +3,16 @@ import sys
 import ast
 
 FILENAME = "example_feature_results.txt"
-FILENAME = "results_ubuntu_11_all_methods_final_1.txt"
+FILENAME = "results_ubuntu_12_all_methods.txt"
+
+features = [1, 2, 3,4,5,6,7,8,9,10,
+                        11,12,13,14,15,16,17,18,19,20,
+                        21,22,23,24,25,26,27,28,29,30,
+                        31,32,33,34,35,36,37,38,39,40,
+                        41,42,43,44,45,46,47,48,49,50,
+                        51,52,53,54,55,56,57];
+
+
 
 def main(argv):
 	data_svm = []
@@ -21,18 +30,19 @@ def main(argv):
 			a_set.add(lamda)
 			accuracy = segs[4]
 			sensitivity = segs[5]
-			w = ast.literal_eval(segs[6].rstrip())
+			classibias = ast.literal_eval(segs[6].rstrip())
 
-			classbia1 = segs[7].rstrip().replace("[", "").replace("]", "")
-			classbia2 = "[" + ",".join(filter(None, classbia1.split(" "))) + "]"
-			classbia3 = ast.literal_eval(classbia2)
+			weights = segs[7].rstrip().replace("[", "").replace("]", "")
+			weights = "[" + ",".join(filter(None, weights.split(" "))) + "]"
+			weights = ast.literal_eval(weights)
+			# print weights
 
 			d = {"features" : features, 
 				"lamda" : lamda, 
-				"accuracy" : accuracy,
-				"sensitivity" : sensitivity,
-				"classibias": classbia3, 
-				"w": w}
+				"accuracy" : float(accuracy),
+				"sensitivity" : float(sensitivity),
+				"weights": weights, 
+				"classibias": float(classibias)}
 
 			if algo == "svm":
 				data_svm.append(d)
@@ -43,15 +53,46 @@ def main(argv):
 			else:
 				print "nothing for ", d
 
-			# print d
-			# break
-		# except ValueError as ex :
-		# 	print ex
-		# 	print "Error - ", line
-		# 	break
-		# data.append(segs)
+	score_features(data_rigde)
+	# print a_set
 
-	print a_set
+def score_features(results):
+	features_scores = {}
+	features_occurence = []
+
+	# Null zero features
+	features_occurence.append(0)
+
+	for f in features:
+		features_scores[str(f)] =  0
+		features_occurence.append(0)
+
+	for point in results:
+		gobal_score = get_score(point['accuracy'], point['sensitivity'], point['classibias'])
+
+		index = 0
+		for f in point['features']:
+			weight = point['weights'][index]
+			# print features_scores
+			score_now = features_scores[str(f)]
+			features_scores[str(f)] = score_now + weight + gobal_score
+			features_occurence[f] = features_occurence[f] + 1
+			index += 1
+
+	#  Averaging the scores according to the occrurance.
+	for f in features:
+		score_now = features_scores[str(f)]
+		features_scores[str(f)] = score_now / features_occurence[f]
+
+	sorted_scores = sorted(features_scores.items(), key=lambda x: x[1])
+	
+	for k in sorted_scores:
+		print "feature - ", k[0], " score ", k[1]
+
+
+
+def get_score(a,s, wo):
+	return a + s+ wo;
 
 if __name__ == "__main__":
 	main(sys.argv)
