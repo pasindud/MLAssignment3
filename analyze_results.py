8c959aa27ce58a89739ff53119e71be2cfa8ecfc
Pasindu De Silva <ppasindud@gmail.com>
2017-02-07 23:12:15 +0800

    added final file results

--- analyze_results.py
+++ analyze_results.py
@@ -3,21 +3,22 @@ import sys
 import ast
 
 FILENAME = "example_feature_results.txt"
-FILENAME = "results_ubuntu_11_all_methods.txt"
+FILENAME = "results_ubuntu_11_all_methods_final_1.txt"
 
 def main(argv):
 	data_svm = []
 	data_logistic = []
 	data_rigde = []
-
+	a_set = set()
 	for line in open(FILENAME):
 		# try:
 			line = line.rstrip()
 			segs = line.split("\t")
 
 			features = ast.literal_eval(segs[1])
-			algo = segs[2]
+			algo = segs[2].strip()
 			lamda = segs[3]
+			a_set.add(lamda)
 			accuracy = segs[4]
 			sensitivity = segs[5]
 			w = ast.literal_eval(segs[6].rstrip())
@@ -33,7 +34,16 @@ def main(argv):
 				"classibias": classbia3, 
 				"w": w}
 
-			print d
+			if algo == "svm":
+				data_svm.append(d)
+			elif(algo == "ridge") :
+				data_rigde.append(d)
+			elif(algo == "logistic"):
+				data_logistic.append(d)
+			else:
+				print "nothing for ", d
+
+			# print d
 			# break
 		# except ValueError as ex :
 		# 	print ex
@@ -41,7 +51,7 @@ def main(argv):
 		# 	break
 		# data.append(segs)
 
-
+	print a_set
 
 if __name__ == "__main__":
 	main(sys.argv)
